12897cd7fa5da3deaff4d907b534cc0fe0d115a1
Patrick Mutua <pawatechsys@gmail.com>
2026-02-21 20:06:47 +0300

    Implement task filtering with List Comprehensions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from tasks.task_manager import display_tasks, filter_tasks, task_generator
 
 # Initial list of tasks
-tasks = ["Buy groceries", "Finish project", "Call mom", "Send email", "Clean room"]
+tasks = ["Buy groceries","Work on Java Project", "Deploy React Project", "Buy fresh Groceries", "Finish project", "Call mom", "Send email", "Clean room"]
 
 print("\nAll Tasks:")
 display_tasks(tasks)
+filter_tasks(tasks, "groceries")
+# Creating a generator for "project" tasks
+project_tasks = task_generator(tasks, "groceries")
+
+# Retrieving tasks lazily
+print(next(project_tasks))  # Output: 'Finish project'
+
+
 
